928ba90ea311ebe6032d0713faf3c27e08d0a776
J <jcnguyen1994@gmail.com>
2018-11-03 22:28:04 -0400

    TestUI
    (Aesthetically Unpleasing)

---
+++ app/src/main/java/com/jgchk/hotelhavoc/ui/main/StartActivity.kt
@@ -0,0 +1,13 @@
+package com.jgchk.hotelhavoc.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jgchk.hotelhavoc.R
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
